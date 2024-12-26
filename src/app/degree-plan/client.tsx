de2b91aaee66d962d0eb2262df9fae1e0f838c78
Vicente Miranda <vmiranda6@gatech.edu>
2024-12-26 12:16:00 -0500

    changes

--- src/app/degree-plan/client.tsx
+++ src/app/degree-plan/client.tsx
@@ -19,7 +19,6 @@ const DegreePlanPageClient: FC<DegreePlanPageClientProps> = ({
 
   const [termsSelected, setTermsSelected] = useState<string[] | null>(null);
   const [showUnselected, setShowUnselected] = useState<boolean>(false);
-  const [hoverRecord, setHoverRecord] = useState<Record<string, boolean>>({});
 
   const { profile, termSelectionsMap } = useProfile();
 
@@ -32,7 +31,6 @@ const DegreePlanPageClient: FC<DegreePlanPageClientProps> = ({
         .sort((a: string, b: string) => termToSortableInteger(a) - termToSortableInteger(b));
       
       setTermsSelected(sortedTerms);
-      setHoverRecord(Object.fromEntries(sortedTerms.map(term => [term, false])));
     }
   }, [termSelectionsMap]);
 
@@ -57,75 +55,78 @@ const DegreePlanPageClient: FC<DegreePlanPageClientProps> = ({
       <div className="w-4/5 mx-auto my-8 flex flex-col gap-16">
         <div className="flex flex-col gap-8 w-full">
           <DegreePlanHeader />
-          <div className="flex flex-row gap-2 flex-wrap">
-            {showUnselected ? (
-              <>
-                {ALL_TERMS.map((term: string) => {
-                  const isSelected = termsSelected!.includes(term);
-                  return (
-                    <div 
-                      className={`flex flex-row h-fit gap-2 border border-gray-300 rounded-lg px-2 py-1 items-center cursor-pointer
-                        ${isSelected ? 'hover:bg-gray-100 bg-white' : 'bg-gray-300 hover:bg-gray-100'}
-                      `}
-                      onClick={() => {
-                        if (isSelected) {
+          <div className="flex flex-col gap-2">
+            <p className="heading-sm text-lg">Terms Selected</p>
+            <div className="flex flex-row gap-2 flex-wrap">
+              {showUnselected ? (
+                <>
+                  {ALL_TERMS.map((term: string) => {
+                    const isSelected = termsSelected!.includes(term);
+                    return (
+                      <div 
+                        className={`flex flex-row h-fit gap-2 border border-gray-300 rounded-lg px-2 py-1 items-center cursor-pointer
+                          ${isSelected ? 'hover:bg-gray-100 bg-white' : 'bg-gray-300 hover:bg-gray-100'}
+                        `}
+                        onClick={() => {
+                          if (isSelected) {
+                            handleUnselectTerm(term);
+                          } else {
+                            handleSelectTerm(term);
+                          }
+                        }}
+                        key={`key-${term}`}
+                      >
+                        {isSelected ? (
+                          <CloseIcon 
+                            style={{ width: '14px', height: '14px' }}
+                          />
+                        ) : (
+                          <AddIcon 
+                            style={{ width: '14px', height: '14px' }}
+                          />
+                        )}
+                        <p className="text-sm">{term}</p>
+                      </div>
+                    )
+                  })}
+                  <p 
+                    className="text-sm px-2 py-1 cursor-pointer text-gray-500 hover:text-gray-700"
+                    onClick={() => {
+                      setShowUnselected(!showUnselected);
+                    }}
+                  >
+                    Show less
+                  </p>
+                </>
+              ) : (
+                <>
+                  {termsSelected?.map((term: string) => {
+                    return (
+                      <div 
+                        className="flex flex-row bg-white border border-gray-300 h-fit gap-2 rounded-lg px-2 py-1 items-center hover:bg-gray-300 cursor-pointer"
+                        onClick={() => {
                           handleUnselectTerm(term);
-                        } else {
-                          handleSelectTerm(term);
-                        }
-                      }}
-                      key={`key-${term}`}
-                    >
-                      {isSelected ? (
+                        }}
+                        key={`key-${term}`}
+                      >
                         <CloseIcon 
                           style={{ width: '14px', height: '14px' }}
                         />
-                      ) : (
-                        <AddIcon 
-                          style={{ width: '14px', height: '14px' }}
-                        />
-                      )}
-                      <p className="text-sm">{term}</p>
-                    </div>
-                  )
-                })}
-                <p 
-                  className="text-sm px-2 py-1 cursor-pointer text-gray-500 hover:text-gray-700"
-                  onClick={() => {
-                    setShowUnselected(!showUnselected);
-                  }}
-                >
-                  Show less
-                </p>
-              </>
-            ) : (
-              <>
-                {termsSelected?.map((term: string) => {
-                  return (
-                    <div 
-                      className="flex flex-row bg-white h-fit gap-2 border border-gray-300 rounded-lg px-2 py-1 items-center hover:bg-gray-100 cursor-pointer"
-                      onClick={() => {
-                        handleUnselectTerm(term);
-                      }}
-                      key={`key-${term}`}
-                    >
-                      <CloseIcon 
-                        style={{ width: '14px', height: '14px' }}
-                      />
-                      <p className="text-sm">{term}</p>
-                    </div>
-                  )
-                })}
-                <p 
-                  className="text-sm px-2 py-1 cursor-pointer text-gray-500 hover:text-gray-700"
-                  onClick={() => {
-                    setShowUnselected(!showUnselected);
-                  }}
-                >
-                  Show more
-                </p>
-              </>
-            )}
+                        <p className="text-sm">{term}</p>
+                      </div>
+                    )
+                  })}
+                  <p 
+                    className="text-sm px-2 py-1 cursor-pointer text-gray-500 hover:text-gray-700"
+                    onClick={() => {
+                      setShowUnselected(!showUnselected);
+                    }}
+                  >
+                    Show all
+                  </p>
+                </>
+              )}
+            </div>
           </div>
         </div>
         <TermGrid 
--- src/app/degree-plan/scheduleDropdown.tsx
+++ src/app/degree-plan/scheduleDropdown.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { FC, useCallback, useMemo, useState } from "react";
+import { FC, Ref, useCallback, useEffect, useMemo, useRef, useState } from "react";
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import CheckIcon from '@mui/icons-material/Check';
 import { Input } from "@nextui-org/input";
@@ -26,6 +26,7 @@ const ScheduleDropdown: FC<ScheduleDropdownProps> = ({
   const [noneSelected, setNoneSelected] = useState<boolean>(selectedOption === '');
   const [activeHelper, setActiveHelper] = useState<string | null>(null);
   const [helperValue, setHelperValue] = useState<string>('');
+  const helperRef = useRef<HTMLInputElement | null>(null);
 
   const onValueChange = (value: string) => {
     setHelperValue(value || '');
@@ -34,6 +35,12 @@ const ScheduleDropdown: FC<ScheduleDropdownProps> = ({
   const onClear = () => {
     setHelperValue('');
   }
+
+  useEffect(() => {
+    if (helperRef && helperRef.current) {
+      helperRef!.current!.focus();
+    }
+  }, [helperRef, activeHelper]);
  
   return (
     <div 
@@ -42,7 +49,7 @@ const ScheduleDropdown: FC<ScheduleDropdownProps> = ({
         setIsOpen(true);
       }}
     >
-      <div className="w-fit flex flex-row gap-2 items-center bg-gray-200 cursor-pointer px-4 py-1 rounded-md hover:bg-gray-300">
+      <div className="flex flex-row gap-2 items-center bg-gray-200 cursor-pointer px-4 py-1 rounded-md hover:bg-gray-300">
         <p className="text-sm">{text}</p>
         <ArrowDropDownIcon 
           style={{ width: '22px', height: '22px', color: noneSelected ? 'gray' : 'default' }}
@@ -50,24 +57,25 @@ const ScheduleDropdown: FC<ScheduleDropdownProps> = ({
         />
       </div>
       {isOpen && (
-        <div className="absolute top-[calc(100%+4px)] overflow-visible left-0 z-20 max-h-60 overflow-y-scroll">
+        <div className="absolute top-[calc(100%+4px)] min-w-56 overflow-visible left-0 z-20 max-h-60 overflow-y-scroll">
           {options.map((option, index) => (
-            <div key={index} className="flex flex-row gap-2 items-center">
+            <div key={index} className="flex flex-row items-end py-1">
               <button
                 key={index}
                 onClick={(e) => {
                   e.stopPropagation();
                   if (option.helper) {
                     setActiveHelper(activeHelper === option.label ? null : option.label);
+                    setHelperValue('');
                   } else {
                     option.onClick();
                     setIsOpen(false);
                     setActiveHelper(null);
                   }
                 }}
-                className={`${selectedOption === option.label ? 'bg-gray-100' : ''} min-w-48 max-w-96 block px-4 py-2 text-left hover:bg-gray-100 text-sm bg-white border border-gray-200 rounded-none shadow-md`}
+                className={`${selectedOption === option.label ? 'bg-gray-100' : ''} min-w-56 px-4 py-2 text-left hover:bg-gray-100 text-sm bg-white border border-gray-200 rounded-none`}
               >
-                <div className="p-0 w-full flex flex-row justify-between gap-4 items-center">
+                <div className="p-0 flex flex-row justify-between gap-4 items-center">
                   {option.customNode || option.label}
                   {selectedOption === option.label && (
                     <CheckIcon 
@@ -77,8 +85,24 @@ const ScheduleDropdown: FC<ScheduleDropdownProps> = ({
                 </div>
               </button>
               {option.helper && activeHelper === option.label && (
-                <div className="z-30 top-0 block min-w-48 bg-white border border-gray-200 shadow-md">
-                  <p className="text-sm mb-2">Enter a schedule name</p>
+                <div className="flex items-center max-h-8">
+                  <div className="h-[2px] w-10 bg-gray-300 top-10" />
+                  <div className="flex flex-row gap-4 bg-white px-4 py-2 whitespace-nowrap items-center border border-gray-200 rounded-none shadow-md">
+                    <p className="text-sm">Enter a schedule name</p>
+                    <Input
+                      isClearable
+                      variant="bordered"
+                      classNames={{
+                        base: "min-w-24",
+                        inputWrapper: "h-5 min-h-5 max-h-5 p-0 border-1 border-t-0 border-l-0 border-r-0 border-default-400 rounded-none group-data-[focus=true]:border-default-400",
+                      }}
+                      placeholder={''}
+                      value={helperValue}
+                      onClear={onClear}
+                      onValueChange={onValueChange}
+                      ref={helperRef}
+                    />
+                  </div>
                 </div>
               )} 
             </div>
@@ -92,6 +116,8 @@ const ScheduleDropdown: FC<ScheduleDropdownProps> = ({
           onClick={(e) => {
             e.stopPropagation();  
             setIsOpen(false);
+            setActiveHelper(null);
+            setHelperValue('');
           }}
         />
       )}
